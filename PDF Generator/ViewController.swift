9e622f7eb2fe1ceaf634238b94959f5796e54a32
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-23 23:39:54 +0530

    pdf from view

--- PDF Generator/ViewController.swift
+++ PDF Generator/ViewController.swift
@@ -13,8 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createPdfFromView(aView: self.view, saveToDocumentsWithFileName: "Prashant.pdf")
     }
 
 
+    func createPdfFromView(aView: UIView, saveToDocumentsWithFileName fileName: String)
+    {
+        let pdfData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData, aView.bounds, nil)
+        UIGraphicsBeginPDFPage()
+        
+        guard let pdfContext = UIGraphicsGetCurrentContext() else { return }
+        
+        aView.layer.render(in: pdfContext)
+        UIGraphicsEndPDFContext()
+        
+        if let documentDirectories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let documentsFileName = documentDirectories + "/" + fileName
+            debugPrint(documentsFileName)
+            pdfData.write(toFile: documentsFileName, atomically: true)
+        }
+    }
+    
 }
 
